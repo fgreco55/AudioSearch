f00041d12ccfc9fa1267086f40feaf1ac2755e88
Frank Greco <fgreco@javasig.com>
2026-04-26 16:20:03 -0400

    Add /topn command that allows the user to control the maximum number of search matches to show

--- src/main/java/com/audiosearch/commands/AppState.java
+++ src/main/java/com/audiosearch/commands/AppState.java
@@ -3,6 +3,7 @@ package com.audiosearch.commands;
 public class AppState {
     private String currentStoreFile = "embedding-store.json";
     private double relevanceThreshold = 0.0;
+    private int topN = 5;
 
     public String getCurrentStoreFile() {
         return currentStoreFile;
@@ -18,5 +19,13 @@ public class AppState {
 
     public void setRelevanceThreshold(double threshold) {
         this.relevanceThreshold = threshold;
+    }
+
+    public int getTopN() {
+        return topN;
+    }
+
+    public void setTopN(int topN) {
+        this.topN = topN;
     }
 }
